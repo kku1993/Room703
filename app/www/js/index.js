daa87d73f52c809c4b171cfb8a645a39d10eb9e0
kku <kku1993dev@gmail.com>
2013-11-19 00:28:55 -0500

    added registration of gcm_reg_id with server

--- app/www/js/index.js
+++ app/www/js/index.js
@@ -18,10 +18,46 @@ var app = {
   // function, we must explicity call 'app.receivedEvent(...);'
   onDeviceReady: function() {
     app.receivedEvent('deviceready');
+
+    /* set up GCM */
+    var pushNotification = window.plugins.pushNotification;
+    pushNotification.register(
+      app.gcmRegisterSuccessful, 
+      app.gcmRegisterError,
+      {"senderID":"769579813594", "ecb":"app.onGCMNotification"});
   },
 
   // Update DOM on a Received Event
   receivedEvent: function(id) {
     console.log('Received Event: ' + id);
+  },
+
+  gcmRegisterSuccessful: function(result){
+    console.log("GCM registration successful: " + result);
+  },
+
+  gcmRegisterError: function(error){
+    console.log("GCM registration error " + error);
+  },
+
+  onGCMNotification: function(e){
+    switch(e.event){
+      case "registered":
+        if(e.regid.length > 0){
+          console.log("Regid " + e.regid);
+          window.localStorage["gcm_reg_id"] = e.regid;
+        }
+        break;
+      case "message":
+        /* actual message delivery */
+        alert('message = ' + e.message + ' msgcnt = ' + e.msgcnt);
+        break;
+      case "error":
+        alert("GCM error " + e.msg);
+        break;
+      default:
+        alert("Unknown GCM event has occurred");
+        break;
+    }
   }
 };
