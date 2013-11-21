d63eb3a7fc15d8099de8dd57f245f48e7d889151
kku <kku1993dev@gmail.com>
2013-11-20 23:35:36 -0500

    client item detail dialog successfully retrieving item information from server

--- app/www/js/util.js
+++ app/www/js/util.js
@@ -1,3 +1,20 @@
-function changePage(url){
-  window.location = url;
+function changePage(url, data){
+  if(data == null)
+    data = {transition: "slide" };
+
+  $.mobile.changePage(url, data);
+}
+
+/* convert item status code to text */
+function itemStatusToText(status_code){
+  switch(status_code){
+    case 0: 
+      return "rejected";
+    case 1:
+      return "pending";
+    case 2:
+      return "accepted";
+    default:
+      return "unknown";
+  }
 }
